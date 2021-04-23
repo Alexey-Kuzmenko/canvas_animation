e9b3a60611c5a9c5e5e44f456b431b25fbb116f5
Alexey Kuzmenko <Alexey.Kuzmenko1101@gmail.com>
2021-04-23 15:54:48 +0300

    add bit of animation code

--- ball_animation.js
+++ ball_animation.js
@@ -1,1 +1,29 @@
-alert("testing")
+// ! connect DOM element 
+const canvas = document.getElementById("animation")
+const ctx = canvas.getContext("2d")
+
+function Ball() {
+    this.x = 100
+    this.y = 100
+    this.xSpeed = -2
+    this.ySpeed = 3
+}
+
+const circle = (x, y, radius, fillCircle) => {
+    ctx.beginPath()
+
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false)
+        if (fillCircle) {
+            ctx.fill()
+        }else {
+            ctx.stroke()
+        }
+}
+Ball.prototype.draw = function () {
+    circle(this.x, this.y, 3, true)
+}
+
+Ball.prototype.move = function () {
+    this.x += this.xSpeed
+    this.y += this.ySpeed
+}
