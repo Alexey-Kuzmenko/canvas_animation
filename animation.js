72227cb32348b4e7db3e5e9b12d83d2630e4d233
Alexey Kuzmenko <Alexey.Kuzmenko1101@gmail.com>
2021-04-23 14:29:16 +0300

    add bee animation

--- animation.js
+++ animation.js
@@ -15,20 +15,78 @@
 
 // * change rect size 
 
+// const canvas = document.getElementById("animation")
+// const ctx = canvas.getContext("2d")
+
+// let rectSize = 0
+
+// setInterval(
+//     function () {
+//         ctx.clearRect(0, 0, 200, 200)
+//         ctx.fillRect(0, 0, rectSize, rectSize)
+        
+//         rectSize++
+//         if (rectSize > 200) {
+//             rectSize = 0
+//         }
+//     },30)
+
+// * bee animation 
+
+const circle = (x, y, radius, fillCircle) => {
+    ctx.beginPath()
+
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false)
+        if (fillCircle) {
+            ctx.fill()
+        }else {
+            ctx.stroke()
+        }
+}
+
+// * draw bee
+
+const drawBee = (x, y) => {
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "Black";
+    ctx.fillStyle = "Gold";
+
+    circle(x, y, 8, true)
+    circle(x, y, 8, false)
+    circle(x - 5, y - 11, 5, false)
+    circle(x + 5, y - 11, 5, false)
+    circle(x - 2, y - 1, 2, false)
+    circle(x + 2, y - 1, 2, false)
+}
+
+// * change bee position 
+const update = (coordinate) => {
+    let offset = Math.random() * 4 - 2
+    coordinate += offset
+
+    if (coordinate > 200) {
+        coordinate = 200
+    } 
+    if (coordinate < 0) {
+        coordinate = 0
+    }
+    return coordinate
+}
+
+// * bee animation code 
 const canvas = document.getElementById("animation")
 const ctx = canvas.getContext("2d")
 
-let rectSize = 0
+let x = 100
+let y = 100
 
 setInterval(
     function () {
         ctx.clearRect(0, 0, 200, 200)
-        ctx.fillRect(0, 0, rectSize, rectSize)
         
-        rectSize++
-        if (rectSize > 200) {
-            rectSize = 0
-        }
-    },30)
-
+        drawBee(x, y)
+        x = update(x)
+        y = update(y)
+        ctx.strokeRect(0, 0, 200, 200)
+    }, 30)
 
