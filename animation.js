5a488f9f4f02b23c9d4a1ff1a9bc778516fa9338
Alexey Kuzmenko <Alexey.Kuzmenko1101@gmail.com>
2021-04-23 12:35:46 +0300

    add animation wich change rect size

--- animation.js
+++ animation.js
@@ -1,17 +1,34 @@
 // alert("testing")
+// const canvas = document.getElementById("animation")
+// const ctx = canvas.getContext("2d")
+
+// let position = 0
+// setInterval(
+//     function () {
+//         ctx.clearRect(0, 0, 200, 200)
+//         ctx.fillRect(position, 0, 20, 20)
+//         position++
+//         if (position > 200) {
+//             position = 0
+//         }
+//     },30)
+
+// * change rect size 
+
 const canvas = document.getElementById("animation")
 const ctx = canvas.getContext("2d")
 
-let position = 0
+let rectSize = 0
+
 setInterval(
     function () {
         ctx.clearRect(0, 0, 200, 200)
-        ctx.fillRect(position, 0, 20, 20)
-
-        position++
-        if (position > 200) {
-            position = 0
+        ctx.fillRect(0, 0, rectSize, rectSize)
+        
+        rectSize++
+        if (rectSize > 200) {
+            rectSize = 0
         }
     },30)
 
-    // ! clear Rect - очистить прямоугольник
+
