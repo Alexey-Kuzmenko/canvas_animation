10f0d70c8b2dffeb3d29d452a39133a5a7d73889
Alexey Kuzmenko <Alexey.Kuzmenko1101@gmail.com>
2021-04-23 10:46:44 +0300

    add rect animation

--- animation.js
+++ animation.js
@@ -1,1 +1,17 @@
-alert("testing")
+// alert("testing")
+const canvas = document.getElementById("animation")
+const ctx = canvas.getContext("2d")
+
+let position = 0
+setInterval(
+    function () {
+        ctx.clearRect(0, 0, 200, 200)
+        ctx.fillRect(position, 0, 20, 20)
+
+        position++
+        if (position > 200) {
+            position = 0
+        }
+    },30)
+
+    // ! clear Rect - очистить прямоугольник
